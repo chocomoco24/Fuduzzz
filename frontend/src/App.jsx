71a93e1674f11b3d539f6d5c0e57416256755c75
chocomoco24 <debarghyadatta2004@gmail.com>
2026-06-26 23:35:26 +0530

    Udhghatan

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <BrowserRouter>
+            <Routes>
+                
+            </Routes>
+        </BrowserRouter>
+    </>
+  )
+}
+
+export default App
